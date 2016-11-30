e62d118a2a4c711837ee6e9e000727d56cff48c2
Jeff Bernt <jbernt@splunk.com>
2016-11-30 09:53:31 -0800

    Added checksum
    Fixed the checksums

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,15 +8,14 @@ $registryUninstallerKeyName = 'splunkforwarder' #ensure this is the value in the
 $installerType = 'MSI'
 $url = 'http://download.splunk.com/products/splunk/releases/6.5.1/universalforwarder/windows/splunkforwarder-6.5.1-f74036626f0c-x86-release.msi'
 $url64 = 'http://download.splunk.com/products/splunk/releases/6.5.1/universalforwarder/windows/splunkforwarder-6.5.1-f74036626f0c-x64-release.msi'
-checksum      = 'c0135f218cea5a6cdeb181b7ef9025cd'
-checksumType  = 'md5'
-checksum64    = '34d4004ffef30b0a286b1dbad467a465'
-checksumType64= '{{ChecksumTypex64}}'
+$checksum      = 'c0135f218cea5a6cdeb181b7ef9025cd'
+$checksumType  = 'md5'
+$checksum64    = '34d4004ffef30b0a286b1dbad467a465'
+$checksumType64= $checksumType
 $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" ["$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
 $packageParameters = $env:chocolateyPackageParameters;
 
 if (!($packageParameters -like "*RECEIVING_INDEXER*") -and !($packageParameters -like "*DEPLOYMENT_SERVER*"))
@@ -25,5 +24,6 @@ if (!($packageParameters -like "*RECEIVING_INDEXER*") -and !($packageParameters 
 }
 Write-Output "Installing with  $packageParameters AGREETOLICENSE=Yes $silentArgs"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$packageParameters AGREETOLICENSE=Yes $silentArgs" "$url" "$url64"
+#Install-ChocolateyPackage "$packageName" "$installerType" "$packageParameters AGREETOLICENSE=Yes $silentArgs" "$url" "$url64"
+Install-ChocolateyPackage "$packageName" "$installerType" "$packageParameters AGREETOLICENSE=Yes $silentArgs" "$url" "$url64" "-validExitCodes $validExitCodes" "-checksum $checksum" "-checksumType $checksumType" "-checksum64 $checksum64" "-checksumType64 $checksumType64"
 
