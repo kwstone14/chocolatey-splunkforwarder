3a7453b56793cb7859de021c85b01d594602aa8f
Jeffrey Bernt <jeff@bernt.net>
2017-07-05 12:19:53 -0700

    Updating to newer package format and updating UF to 6.6.2

--- testall.cmd
+++ testall.cmd
@@ -1,5 +1,10 @@
 del *.nupkg
 choco pack
-choco install splunkforwarder  -s '%cd%' -f -y
+choco install -dv -s '%cd%' -f -y --skip-virus-check splunk-universalforwarder
 pause
-choco uninstall splunkforwarder -y
+choco uninstall -dv -y splunk-universalforwarder
+REM pause
+REM choco install -s '%cd%' -f -y --x86 --skip-virus-check splunk-universalforwarder
+REM pause
+REM choco uninstall -y splunk-universalforwarder
+REM pause
