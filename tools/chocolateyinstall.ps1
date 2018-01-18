b24d40c7de6d13c7b95b0c716b62c5376aef13fb
Jeffrey Bernt <dagryph@users.noreply.github.com>
2018-01-18 14:01:54 -0800

    Updating to 7.0.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86&platform=windows&version=6.6.3&product=universalforwarder&filename=splunkforwarder-6.6.3-e21ee54bc796-x86-release.msi&wget=true' # download url, HTTPS preferred
-$url64 = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=6.6.3&product=universalforwarder&filename=splunkforwarder-6.6.3-e21ee54bc796-x64-release.msi&wget=true' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86&platform=windows&version=7.0.1&product=universalforwarder&filename=splunkforwarder-7.0.1-2b5b15c4ee89-x86-release.msi&wget=true' # download url, HTTPS preferred
+$url64 = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.0.1&product=universalforwarder&filename=splunkforwarder-7.0.1-2b5b15c4ee89-x64-release.msi&wget=true' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
     packageName    = $packageName
@@ -13,9 +13,9 @@ $packageArgs = @{
     url64bit       = $url64
 
     softwareName   = 'SplunkUniversalForwarder'
-    checksum       = 'fc3cd256c61fe26688cf49173920e0b7'
+    checksum       = '88100d8b6c2d8b5ca9832404d3f1c7b2'
     checksumType   = 'md5'
-    checksum64     = '830c54b695e297f92c1d94122db753f3'
+    checksum64     = '5fee4fbfd9d640153ce5c7ad24e90b7a'
     checksumType64 = $checksumType
 
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AGREETOLICENSE=YES" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
