98ce017281d4566adc26ed6884b85e8113a8490b
Jeffrey Bernt <dagryph@users.noreply.github.com>
2018-01-18 14:36:04 -0800

    Update testall.cmd

--- testall.cmd
+++ testall.cmd
@@ -1,10 +1,4 @@
-del *.nupkg
-choco pack
 choco install -dv -source "'.;https://chocolatey.org/api/v2/'" -f -y --skip-virus-check splunk-universalforwarder
-pause
 choco uninstall -dv -y splunk-universalforwarder
-pause
 REM choco install -source "'.;https://chocolatey.org/api/v2/'" -f -y --x86 --skip-virus-check splunk-universalforwarder
-REM pause
 REM choco uninstall -y splunk-universalforwarder
-REM pause
